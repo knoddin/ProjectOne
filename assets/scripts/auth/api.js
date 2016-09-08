6449031fcc11fc6d2e436de239e58f37a7c74f80
knoddin <knoddin@gmail.com>
2016-09-08 10:58:10 -0400

    authentication. copied and pasted.

---
+++ assets/scripts/auth/api.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const app = require('../app');
+
+const signUp = (data) => {
+  return $.ajax({
+    url: app.host + '/sign-up',
+    method: 'POST',
+    data: data,
+  });
+};
+
+const signIn = (data) => {
+  console.log("I am in sign in");
+  return $.ajax({
+    url: app.host + '/sign-in',
+    method: 'POST',
+    data: data,
+  });
+};
+
+const changePassword = (data) => {
+  return $.ajax({
+    url: app.host + '/change-password/' + app.user.id,
+    method: 'PATCH',
+    headers: {
+      Authorization: 'Token token=' + app.user.token,
+    },
+    data: data,
+  });
+};
+
+const signOut = () => {
+  return $.ajax({
+    url: app.host + '/sign-out/' + app.user.id,
+    method: 'DELETE',
+    headers: {
+      Authorization: 'Token token=' + app.user.token,
+    }
+  });
+};
+
+module.exports = {
+  signUp,
+  signIn,
+  changePassword,
+  signOut
+};
