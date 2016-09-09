214d6cf5db3516b82976621b016c31bafe14ec3f
knoddin <knoddin@gmail.com>
2016-09-09 12:39:34 -0400

    board created! it's clickable and fills an array.

--- assets/scripts/maingame.js
+++ assets/scripts/maingame.js
@@ -2,50 +2,44 @@
 
 const getFormFields = require('../../lib/get-form-fields');
 
-let boardArray = [0,1,2,3,4,5,6,7,8];
-let newBoard = [];
+let gameBoard = [" "," "," "," "," "," "," "," "," "];
+let turn = 0;
 let playerX = 1;
+let currentTile;
 
 let gameboardID = document.getElementById("game-board");
 
+
 const createBoard = function() {
-  for (let i = 0; i < boardArray.length; i++) {
+  for (let i = 0; i < gameBoard.length; i++) {
     let newTileDiv = document.createElement('div');
     newTileDiv.className = 'tile';
-    newTileDiv[i] = boardArray[i];
-    newTileDiv.addEventListener('click', isXO);
+    newTileDiv.setAttribute('data-id', i);
     gameboardID.appendChild(newTileDiv);
-    console.log("I am in createBoard");
+    newTileDiv.addEventListener('click', onClick);
   }
 };
 
-// const onTileClick = function(event){
-//   event.preventDefault();
-//   let data= getFormFields(event.target);
-//   if (playerX === 1) {
-//     this.innerHTML = "X";
-//     gameObj.board.push($);
-//     playerX = 0;
-//   }
-//     else {
-//       this.innerHTML = "O";
-//       playerX = 1;
-//     }
-// };
-
-const isXO = function(event){
+const onClick = function(event){
   event.preventDefault();
+  let tileClicked = $(this).attr('data-id');
   if (playerX === 1) {
     this.innerHTML = "X";
+    gameBoard.insert(tileClicked, "X");
     playerX = 0;
   }
   else {
     this.innerHTML = "O";
+    gameBoard.insert(tileClicked, "O");
     playerX = 1;
   }
 };
 
+Array.prototype.insert = function (index, item) {
+  this.splice(index, 0, item);
+};
+
 module.exports = {
   createBoard,
-  isXO,
+  onClick,
 };
