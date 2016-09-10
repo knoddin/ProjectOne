9e91ed11db4f0653d41f9027756397789c69b7b7
knoddin <knoddin@gmail.com>
2016-09-10 16:00:52 -0400

    index.html added new game button and started clear board process.

--- assets/scripts/maingame.js
+++ assets/scripts/maingame.js
@@ -2,6 +2,7 @@
 
 const getFormFields = require('../../lib/get-form-fields');
 
+
 let gameBoard = ["","","","","","","","",""];
 let player = 0;
 let turns = 0;
@@ -18,17 +19,19 @@ const createBoard = function() {
   }
 };
 
-const onClick = function(event){
+const onClick = function(){
   let tileClicked = $(this).attr('data-id');
   if (player === 1) {
     this.innerHTML = "X";
     gameBoard.insert(tileClicked, "X");
     player = 0;
+    turns += 1;
   }
   else if (player === 0) {
     this.innerHTML = "O";
     gameBoard.insert(tileClicked, "O");
     player = 1;
+    turns += 1;
   }
 };
 
@@ -38,45 +41,43 @@ Array.prototype.insert = function (index, item) {
 
 Array.prototype.clear = function(index, item) {
   this.splice(index, 0, item);
-}
+};
 
 
-const winCondition = function(gameBoard){
+const winCondition = function(input){
   //across
-  if (((gameBoard[0] === gameboard[2]) && (gameBoard[1] === gameBoard[2]))||
-  ((gameBoard[3] === gameboard[5]) && (gameBoard[3] === gameBoard[4])) ||
-  ((gameBoard[6] === gameboard[8]) && (gameBoard[6] === gameBoard[7]))) {
-  this.wins=true;
+  if (((input[0] === input[2]) && (input[1] === input[2]))||
+  ((input[3] === input[5]) && (input[3] === input[4])) ||
+  ((input[6] === input[8]) && (input[6] === input[7]))) {
+  return true;
   }
 //down
-  else if (((gameBoard[0] === gameboard[6]) && (gameBoard[0] === gameBoard[3])) ||
-  ((gameBoard[1] === gameboard[7]) && (gameBoard[1] === gameBoard[4])) ||
-  ((gameBoard[2] === gameboard[5]) && (gameBoard[2] === gameBoard[8]))) {
-  this.wins=true;
+  else if (((input[0] === input[6]) && (input[0] === input[3])) ||
+  ((input[1] === input[7]) && (input[1] === input[4])) ||
+  ((input[2] === input[5]) && (input[2] === input[8]))) {
+  return true;
   }
   //diagnal
-  else if (((gameBoard[0] === gameboard[8]) && (gameBoard[0] === gameBoard[4])) ||
-  ((gameBoard[2] === gameboard[6]) && (gameBoard[2] === gameBoard[4]))) {
-  this.wins=true;
+  else if (((input[0] === input[8]) && (input[0] === input[4])) ||
+  ((input[2] === input[6]) && (input[2] === input[4]))) {
+  return true;
   }
   else if (gameBoard.length = 9) {
   return false;
   }
 }
 
-
-const endGame = function() {
-  document.getElementById("tile").innerHTML = "";
-  for (let i = 0; i < gameBoard.length; i++){
-  gameBoard.clear(i, "");
-  }
-};
+//
+// const endGame = function() {
+//   document.getElementsByClassName("tile").innerHTML = " ";
+//   }
+// };
 
 module.exports = {
   createBoard,
   onClick,
   getFormFields,
-  endGame
+  // endGame
   // winCondition,
   // clearBoard,
 };
